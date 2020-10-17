c94b74d0b0362ca5123198dfaec6eee6cd5c3f1e
Florian Fülling <mail@code-fox.de>
2020-10-17 02:24:06 +0200

    add new command system

---
+++ src/main/java/de/eldoria/eldoworldcontrol/command/WorldControlCommand.java
@@ -0,0 +1,20 @@
+package de.eldoria.eldoworldcontrol.command;
+
+import de.eldoria.eldoutilities.localization.ILocalizer;
+import de.eldoria.eldoutilities.messages.MessageSender;
+import de.eldoria.eldoutilities.simplecommands.EldoCommand;
+import de.eldoria.eldoworldcontrol.core.EldoWorldControl;
+import de.eldoria.eldoworldcontrol.command.worldcontrol.About;
+import de.eldoria.eldoworldcontrol.command.worldcontrol.Reload;
+import de.eldoria.eldoworldcontrol.command.worldcontrol.Verbose;
+import de.eldoria.eldoworldcontrol.core.permissions.PermissionVerboseLogger;
+import org.bukkit.plugin.Plugin;
+
+public class WorldControlCommand extends EldoCommand {
+    public WorldControlCommand(Plugin plugin, PermissionVerboseLogger logger) {
+        super(ILocalizer.getPluginLocalizer(EldoWorldControl.class), MessageSender.get(EldoWorldControl.class));
+        registerCommand("verbose", new Verbose(localizer(), messageSender(), logger));
+        registerCommand("about", new About(localizer(), messageSender(), plugin));
+        registerCommand("reload", new Reload(localizer(), messageSender(), plugin));
+    }
+}
---
+++ src/main/java/de/eldoria/eldoworldcontrol/command/worldcontrol/Reload.java
@@ -0,0 +1,26 @@
+package de.eldoria.eldoworldcontrol.command.worldcontrol;
+
+import de.eldoria.eldoutilities.localization.ILocalizer;
+import de.eldoria.eldoutilities.messages.MessageSender;
+import de.eldoria.eldoutilities.simplecommands.EldoCommand;
+import de.eldoria.eldoworldcontrol.core.EldoWorldControl;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+public class Reload extends EldoCommand {
+    private final EldoWorldControl plugin;
+
+    public Reload(ILocalizer localizer, MessageSender messageSender, Plugin plugin) {
+        super(localizer, messageSender);
+        this.plugin = (EldoWorldControl) plugin;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        plugin.reload();
+        messageSender().sendLocalizedMessage(sender, "Reload complete.");
+        return true;
+    }
+}
