2a8c59c81d7e17620a00faf71f3e7bd491638786
Florian Fülling <mail@code-fox.de>
2020-10-19 18:29:16 +0200

    add permission space

--- src/main/java/de/eldoria/eldoworldcontrol/core/config/General.java
+++ src/main/java/de/eldoria/eldoworldcontrol/core/config/General.java
@@ -14,6 +14,7 @@ import java.util.Map;
 public class General implements ConfigurationSerializable {
     private boolean debug = false;
     private String language = "en_US";
+    private String permissionSpace = "worldcontrol";
 
     public General() {
     }
@@ -22,6 +23,7 @@ public class General implements ConfigurationSerializable {
         TypeResolvingMap map = SerializationUtil.mapOf(objectMap);
         debug = map.getValueOrDefault("debug", debug);
         language = map.getValueOrDefault("language", language);
+        permissionSpace = map.getValueOrDefault("permissionSpace", permissionSpace);
     }
 
     @Override
@@ -29,6 +31,7 @@ public class General implements ConfigurationSerializable {
         return SerializationUtil.newBuilder()
                 .add("debug", debug)
                 .add("language", language)
+                .add("permissionSpace", permissionSpace)
                 .build();
     }
 }
